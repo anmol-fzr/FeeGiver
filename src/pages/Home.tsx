54fd596c418108312b8819bb9636e2c068a4d924
Anmol <ainsa2279@gmail.com>
2024-10-30 20:49:14 +0530

    ✨ Added Global Settings for form opening & env validation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import { API } from "@/services";
 import { PageHeader } from "@/components";
 import { OpenFormAlert, FeesTable } from "@/components";
+import { useEffect, useState } from "react";
+import { ref, onValue } from "firebase/database";
+import { fbRealTimeDB } from "@/config/fbConfig";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const HomePage = () => {
+  const [settings, setSettings] = useState<Record<string, boolean>>({});
+  const [animateRef] = useAutoAnimate();
+
+  useEffect(() => {
+    const settingsRef = ref(fbRealTimeDB);
+    onValue(settingsRef, (snapshot) => {
+      const data = snapshot.val();
+      setSettings(data.settings);
+    });
+  }, []);
+
   const { data } = useQuery({
     queryFn: API.PROFILE.GET,
     queryKey: ["PROFILE"],
@@ -13,8 +28,8 @@ const HomePage = () => {
   const name = data?.data.details.name;
 
   return (
-    <div className="w-full space-y-4">
-      <OpenFormAlert name={name} />
+    <div className="w-full space-y-4" ref={animateRef}>
+      {settings.isFormOpen && <OpenFormAlert name={name} />}
       <PageHeader
         title={`Welcome ${name}`}
         desc="These are your Previously filled fee data"
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,2 +1,3 @@
 export * from "./authSchema";
 export * from "./feeSchema";
+export * from "./envSchema";
