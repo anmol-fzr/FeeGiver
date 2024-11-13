6baf799c6677a1d02c9cc787105bfc374d19ef7b
Anmol <ainsa2279@gmail.com>
2024-11-14 02:39:13 +0530

    Improved 404 Page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,14 @@
-const NotFoundPage = () => <div>404</div>;
+import { Link, PageHeader } from "@/components";
+import { TriangleAlert } from "lucide-react";
+
+const NotFoundPage = () => (
+  <div className="flex flex-col items-center gap-2">
+    <TriangleAlert size={96} />
+    <PageHeader title="Page Not Found" desc="Looks like you are lost" />
+    <div className="mt-8">
+      <Link to="/">Back to Home</Link>
+    </div>
+  </div>
+);
 
 export { NotFoundPage };
